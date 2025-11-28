6e3c1ccea42cfd3714297d1cbbc6144df765e23b
Chris Hughes <naokotani@protonmail.com>
2025-11-28 14:19:40 -0400

    Quadlet post

--- static/js/menu.js
+++
@@ -1,8 +0,0 @@
-function handleSocial() {
-  const div = document.getElementById("social-menu");
-  if (div.style.display === "block") {
-    div.style.display = "none";
-  } else {
-    div.style.display = "block";
-  }
-}
